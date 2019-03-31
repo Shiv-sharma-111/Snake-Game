780c2cc0320a29481d591624b111cf4a6de5fe0f
Shiv Sharma <shivsharmavictory@gmail.com>
2019-03-31 16:34:33 +0530

    Update

--- Snake.js
+++ Snake.js
@@ -6,29 +6,50 @@ function init() {
   pen = canvas.getContext('2d');
   W = canvas.width;
   H = canvas.height;
+  snake ={
+    init_length:5,
+    color:"yellow",
+    cells:[],
+    direction:"right",
+    createSnake:function()
+    {
+      for(var i=this.init_length-1;i>=0;i--)
+      {
+        this.cells.push({x:i,y:0});
+      }
+    },
+    drawSnake:function()
+    {
+      for(var i=0;i<this.cells.length;i++)
+      {
+        pen.fillStyle=this.color;
+        pen.strokeStyle="black";
+        pen.lineWidth=5;
+        pen.strokeRect(this.cells[i].x*10,this.cells[i].y*10,10,10);
+        pen.fillRect(this.cells[i].x*10,this.cells[i].y*10,10,10);
+      }
+    },
+    updateSnake:function()
+    {
+      var headX= this.cells[0].x;
+      var headY= this.cells[0].y;
+      //Assuming Snake is moving right
+      //Insertion at headX
+      nextHeadX=headX+1;
+      this.cells.pop();
+      this.cells.unshift({x:nextHeadX,y:headY});
+    }
 
-  box = {
-    x:10,
-    y:20,
-    w:20,
-    h:20,
-    speed:5,
-  }
+  };
+  snake.createSnake();
 
 }
 function draw() {
   pen.clearRect(0,0,W,H);
-  console.log("draw");
-  pen.fillStyle="green";
-  pen.fillRect(box.x,box.y,box.w,box.h);
+  snake.drawSnake();
 }
 function update() {
-  console.log("Update");
-  box.x+=box.speed;
-  if(box.x>=W)
-  {
-    box.speed*=-1;
-  }
+  snake.updateSnake();
 }
 function gameLoop() {
   draw();
